41a50d5146742c04a5f610fba07c05bb6aad57b5
dexthedino <dexter@maher-family.net>
2019-07-01 19:22:45 -0700

    somewhat added timer

--- src/routes/GameRoute/GameRoute.js
+++ src/routes/GameRoute/GameRoute.js
@@ -5,20 +5,47 @@ class GameRoute extends Component {
   state = {
     isGameOver: false,
     cellClickCounter: 0,
+    shouldCount : false,
   };
   handleOnClick = () => {
     console.log('[DX][GameRoute] ouch');
   };
   handleOnCellClick = () => {
     this.setState({ cellClickCounter: this.state.cellClickCounter + 1 });
+    if (this.state.cellClickCounter > 0) {
+      return
+    }else{
+      this.stopWatch();
+    }
   };
+
+  stopWatch=()=>{
+    const {shouldCount} = this.state;
+    // let shouldCount = false;
+    this.setState({shouldCount: !shouldCount})
+    console.log('[DX][GameRoute] shouldCount', shouldCount);
+    if (shouldCount) {
+      this.startCount();  
+    }else{
+      return;
+    }
+    
+  }
+  
+  startCount=()=>{
+    const startTime = Date.now();
+    setTimeout(() => {console.log('[DX][GameRoute] Date.now() - startTime', Date.now() - startTime)}, 3000)
+
+  }
+
+
   render() {
     const { handleOnClick, handleOnCellClick } = this;
     const { cellClickCounter } = this.state;
     return (
       <div className={styles.root}>
         {this.renderGameOver()}
-        <button>Restart Game</button>
+        <button onClick={this.stopWatch}>PAUSE / PLAY</button>
         <div onClick={handleOnClick}>{cellClickCounter}</div>
         <GameBoard onCellClick={handleOnCellClick} boardSize={10} />
       </div>
